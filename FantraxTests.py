05fc704da265343c67493900da2fe4347aa77799
Steve Kiebles <kiebles1@gmail.com>
2022-01-22 15:26:12 -0500

    Basic arb sheet creation. Test writing dicts to sheets.
    Can create the arbitration sheets for each team in google sheets, but don't populate them with anything yet. Added a unit test for writing dictionaries to google sheets.

--- FantraxTests.py
+++ FantraxTests.py
@@ -1,5 +1,7 @@
 import unittest
 import FantraxUtils.FantraxUtils as ftu
+import FantraxTools as ftools
+import UpdateTeamSheets.src.sheets_connect as sc
 import os, os.path
 
 class TestFantraxUtils(unittest.TestCase):
@@ -11,6 +13,18 @@ class TestFantraxUtils(unittest.TestCase):
             len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))]) == 12
         )
         
+class TestFantraxTools(unittest.TestCase):
+    def testCreateArbSheets(self):
+        ftools.create_arb_sheets()
+
+class TestSheetsService(unittest.TestCase):
+    def testWriteDictToSheet(self):
+        guy = {'name':'Mike Trout','cost':'21', 'pos':'OF'}
+        guy = [list(guy.keys()), list(guy.values())]
+        service = sc.SheetsService()
+        wsid = service.create_worksheet('TestSheet')
+        service.add_sheet(wsid, 'test1')
+        service.write_sheet(wsid, 'test1', guy)
 
 if __name__ == '__main__':
     unittest.main()
--- FantraxTools.py
+++ FantraxTools.py
@@ -1,8 +1,39 @@
 from UpdateTeamSheets.src.sheets_connect import SheetsService
 import FantraxUtils.FantraxUtils as FantraxUtils
+import datetime
 
-def create_arb_sheets():
-    print('not ready...')
+def create_arb_workbook(teamName):
+    service = SheetsService()
+    workbookId = service.create_worksheet(teamName + '_' + str(datetime.date.today().year))
+    return workbookId
+
+def populate_team_sheet(teamName, teamId, workbookId):
+    print('not ready')
+
+def write_player_to_team_sheet(teamName, teamId, workbookId, player):
+    service = SheetsService()
+    service.write_sheet(workbookId, teamName, player)
+
+def add_team_to_arb_workbook(teamName, teamId, workbookId):
+    service = SheetsService()
+    service.add_sheet(workbookId, teamName)
+
+def create_arb_workbooks():
+    pairsList = FantraxUtils.get_team_name_id_pairs('./FantraxUtils/cfg/Teams.csv')
+    for outerPair in pairsList:
+        outerName = outerPair[0]
+        outerId = outerPair[1]
+        if outerName == 'Team Name':
+            continue
+
+        currentWorkbookId = create_arb_workbook(outerName)
+
+        for innerPair in pairsList:
+            innerName = innerPair[0]
+            innerId = innerPair[1]
+            # teams don't need to do their own arb
+            if (innerName == 'Team Name') or (innerName == outerName):
+                continue
 
 if __name__ == '__main__':
     FantraxUtils.download_teams_to_sheets()
